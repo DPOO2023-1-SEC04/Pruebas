05f956f011f55a262bae665bf53efb5827d8bce5
Daniel Piñeros <89279439+dpin79@users.noreply.github.com>
2023-03-31 13:45:10 -0500

    Create ReaderFile.java

---
+++ ReaderFile.java
@@ -0,0 +1,11 @@
+import java.io
+public class Main{
+
+  String x = "Test para probar la lectura de archivos"; 
+
+
+public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
